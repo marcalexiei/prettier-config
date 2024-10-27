d0e94daad73efa96c25f42d22445248b99ee511c
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2024-10-27 09:25:15 +0100

    fix: export js files to include config programmatically (#10)

---
+++ index.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const CONFIG = {
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  quoteProps: 'as-needed',
+};
+
+export default CONFIG;
