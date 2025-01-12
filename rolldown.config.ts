22b4fad3a36a3755789fd0f033dee41690e09d96
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-01-12 16:45:13 +0100

    fix(types): config has `any` type (#20)

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,20 +1,27 @@
+import fs from 'node:fs/promises';
+
 import { defineConfig } from 'rolldown';
-import UnpluginIsolatedDecl from 'unplugin-isolated-decl/rolldown';
 
 export default defineConfig([
   {
     input: 'index.js',
-    output: {
-      entryFileNames: '[name].js',
-      format: 'esm',
-    },
-    plugins: [UnpluginIsolatedDecl({ include: 'index.js' })],
-  },
-  {
-    input: 'index.js',
-    output: {
-      entryFileNames: '[name].cjs',
-      format: 'cjs',
-    },
+    output: [
+      {
+        entryFileNames: '[name].js',
+        format: 'esm',
+      },
+      {
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      {
+        name: 'clean-dist',
+        async buildStart() {
+          await fs.rm('./dist', { recursive: true, force: true });
+        },
+      },
+    ],
   },
 ]);
